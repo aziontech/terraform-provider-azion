a9231a2c47748c5881cf93dacc9682db54ed8411
diegojc <diego.jcgarcia@gmail.com>
2023-02-13 16:04:09 -0300

    [ENG-24055] feat: starting initial basecode terraform-provider-azion

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
-		Address: "hashicorp.com/azion/azion-pf",
+		Address: "hashicorp.com/dev/azion",
 	})
 }
