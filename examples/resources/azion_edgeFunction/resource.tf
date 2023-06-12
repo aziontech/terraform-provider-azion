998b8e898f288086860282d415ce96a014c49f07
diegoazion <diego.garcia@azion.com>
2023-06-12 10:11:43 -0300

    feat: Adjusting data source and resource for docs #none

--- examples/resources/azion_edgeFunction/resource.tf
+++ examples/resources/azion_edgeFunction/resource.tf
@@ -1,12 +1,7 @@
-resource "local_file" "content_file" {
-  filename = "${path.module}/example.txt"
-  content  = file("${path.module}/example.txt")
-}
-
 resource "azion_edge_function" "example" {
   edge_function = {
     name           = "Function Terraform Example"
-    code =         local_file.content_file.content or file("${path.module}/example.txt")
+    code           = file("${path.module}/example.txt")
     language       = "javascript"
     initiator_type = "edge_application"
     json_args      = jsonencode(
