a7b570e6d154ced0567b81d66c14ba3a05433535
Vander Vieira <vandervsvieira@gmail.com>
2023-08-09 16:32:19 -0300

    fix domain resource example

--- examples/resources/azion_domain/resource.tf
+++ examples/resources/azion_domain/resource.tf
@@ -6,8 +6,8 @@ resource "azion_domain" "example" {
     ]
     name                   = "Terraform-domain-example"
     digital_certificate_id = null
-    cname_access_only      = true / false
+    cname_access_only      = false
     edge_application_id    = 1234567890
-    is_active              = true / false
+    is_active              = true
   }
 }
