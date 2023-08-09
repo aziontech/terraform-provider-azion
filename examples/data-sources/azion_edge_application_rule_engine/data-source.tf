451d905f21a1732adbd430ad355bed3f360db625
Vander Vieira <vandervsvieira@gmail.com>
2023-08-09 14:03:02 -0300

    fix syntax .tf fmt

--- examples/data-sources/azion_edge_application_rule_engine/data-source.tf
+++ examples/data-sources/azion_edge_application_rule_engine/data-source.tf
@@ -2,6 +2,6 @@ data "azion_edge_application_rule_engine" "example" {
   edge_application_id = 1234567890
   results = {
     phase = "request"
-    id = 123456
+    id    = 123456
   }
 }
--- examples/data-sources/azion_edge_application_rules_engine/data-source.tf
+++ examples/data-sources/azion_edge_application_rules_engine/data-source.tf
@@ -1,6 +1,6 @@
 data "azion_edge_application_rules_engine" "example" {
   edge_application_id = 1234567890
-  results =  [{
+  results = [{
     phase = "request"
   }]
 }
