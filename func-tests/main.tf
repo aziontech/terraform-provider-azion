eb72855a07d543b4e10ab69f681f58d66e04ad8f
Vander Vieira <vandervsvieira@gmail.com>
2023-09-25 19:41:26 -0300

    comment func test

--- func-tests/main.tf
+++ func-tests/main.tf
@@ -69,34 +69,39 @@ resource "azion_edge_application_cache_setting" "testfunc" {
   ]
 }
 
-resource "azion_edge_application_rule_engine" "testfunc" {
-  edge_application_id = azion_edge_application_main_setting.testfunc.edge_application.application_id
-  results = {
-    name        = "Terraform Rule Engine test-func"
-    phase       = "request"
-    description = "My rule engine"
-    behaviors = [
-      {
-        name   = "add_request_header",
-        "target_object" : {
-          "target" = "X-Cache: 100"
-        }
-      }
-    ]
-    criteria = [
-      {
-        entries = [
-          {
-            variable    = "$${uri}"
-            operator    = "is_equal"
-            conditional = "if"
-            input_value = "/"
-          }
-        ]
-      }
-    ]
-  }
-}
+# resource "azion_edge_application_rule_engine" "testfunc" {
+#   edge_application_id = azion_edge_application_main_setting.testfunc.edge_application.application_id
+#   results = {
+#     name        = "Terraform Rule Engine test-func"
+#     phase       = "request"
+#     description = "My rule engine"
+#     behaviors = [
+#       {
+#         name = "capture_match_groups"
+#         target_object = {
+#           regex = "2378",
+#           captured_array = "Terraform",
+#           subject = "$${device_group}"
+#         }
+#       }
+#     ]
+#     criteria = [
+#       {
+#         entries = [
+#           {
+#             variable= "$${uri}"
+#             operator= "is_equal"
+#             conditional= "if"
+#             input_value= "/"
+#           }
+#         ]
+#       }
+#     ]
+#   }
+#   depends_on = [
+#     azion_edge_application_main_setting.testfunc
+#   ]
+# }
 
 resource "azion_edge_function" "testfunc" {
   edge_function = {
@@ -135,6 +140,9 @@ resource "azion_domain" "testfunc" {
     edge_application_id    = azion_edge_application_main_setting.testfunc.edge_application.application_id
     is_active              = true
   }
+  depends_on = [
+    azion_edge_application_main_setting.testfunc
+  ]
 }
 
 resource "azion_edge_firewall_main_setting" "testfunc" {
@@ -185,6 +193,7 @@ resource "azion_intelligent_dns_record" "testfunc" {
     description = "This is a description"
     ttl         = 20
   }
+  depends_on = [ azion_intelligent_dns_zone.testfunc ]
 }
 
 # ---------------------- DATA SOURCES ----------------------
@@ -227,13 +236,13 @@ data "azion_edge_application_rules_engine" "example" {
   }]
 }
 
-data "azion_edge_application_rule_engine" "example" {
-  edge_application_id = azion_edge_application_main_setting.testfunc.edge_application.application_id
-  results = {
-    phase = "request"
-    id    = azion_edge_application_rule_engine.testfunc.results.id
-  }
-}
+# data "azion_edge_application_rule_engine" "example" {
+#   edge_application_id = azion_edge_application_main_setting.testfunc.edge_application.application_id
+#   results = {
+#     phase = "request"
+#     id    = azion_edge_application_rule_engine.testfunc.results.id
+#   }
+# }
 
 data "azion_edge_functions" "example" {
 }
