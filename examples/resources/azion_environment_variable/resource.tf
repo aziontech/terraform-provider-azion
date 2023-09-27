b196e3120402e4635852e6bdf9e7fe8e3e0d0e11
diegoazion <diego.garcia@azion.com>
2023-09-27 11:50:00 -0300

    feat: support to environment variable API #minor

--- examples/resources/azion_environment_variable/resource.tf
+++ examples/resources/azion_environment_variable/resource.tf
@@ -1,7 +1,7 @@
 resource "azion_environment_variable" "example" {
   result = {
-    key                       = "key-test Terraform"
-    value                  = "key-test Terraform"
-    secret     = false
+    key    = "key-test Terraform"
+    value  = "key-test Terraform"
+    secret = false
   }
 }
