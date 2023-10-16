8efeaf5939212bc912795b704461a8074f485875
diegoazion <diego.garcia@azion.com>
2023-10-16 16:07:13 -0300

    feat: support to waf Domains and Events #minor

--- examples/data-sources/azion_waf_domains/data-source.tf
+++ examples/data-sources/azion_waf_domains/data-source.tf
@@ -1,5 +1,5 @@
 data "azion_waf_domains" "example" {
   page      = 1
   page_size = 10
-  waf_id = 5793
+  waf_id    = 5793
 }
--- examples/data-sources/azion_waf_events/data-source.tf
+++ examples/data-sources/azion_waf_events/data-source.tf
@@ -1,7 +1,7 @@
 data "azion_waf_events" "example" {
   #    page = 1
   #    page_size = 30
-  waf_id = 5793
-  domains_ids= [1683058761,1697207627]
-  hour_range=6 #hour_rage [1,3,6,9,12,15,18,21,24,27,30....72]
+  waf_id      = 5793
+  domains_ids = [1683058761, 1697207627]
+  hour_range  = 6 #hour_rage [1,3,6,9,12,15,18,21,24,27,30....72]
 }
