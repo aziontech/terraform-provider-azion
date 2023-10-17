40498afa90730225fb198e53ff2bd9d697195eaa
Diego Garcia <diego.garcia@azion.com>
2023-10-17 09:09:24 -0300

    feat: support to resource for Edge Firewall Functions Instance #patch

--- examples/resources/azion_edge_application_edge_functions_instance/resource.tf
+++ examples/resources/azion_edge_application_edge_functions_instance/resource.tf
@@ -1,9 +1,11 @@
-resource "azion_edge_firewall_edge_functions_instance" "example" {
-  edge_firewall_id = 12464
+resource "azion_edge_application_edge_functions_instance" "example" {
+  edge_application_id = 1234567890
   results = {
-    name = "Terraform Test"
-    "edge_function_id" : 9359
+    name = "Terraform Example"
+    "edge_function_id" : 12345,
     "args" : jsonencode(
-    { a = "b" })
+      { "key"     = "Value",
+        "Example" = "example"
+    })
   }
 }
