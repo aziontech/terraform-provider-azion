2e9594eea714ab1d77742b68af552a10032a014a
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2025-10-21 11:11:43 -0300

    fix: update edge functions instance examples to match schema

--- examples/resources/azion_edge_application_edge_functions_instance/resource.tf
+++ examples/resources/azion_edge_application_edge_functions_instance/resource.tf
@@ -1,11 +1,12 @@
 resource "azion_edge_application_edge_functions_instance" "example" {
-  edge_application_id = 1234567890
-  results = {
-    name = "Terraform Example"
-    "edge_function_id" : 12345,
-    "args" : jsonencode(
-      { "key"     = "Value",
-        "Example" = "example"
+  application_id = "1234567890"
+  data = {
+    name        = "Terraform Example"
+    function_id = 12345
+    active      = true
+    args = jsonencode({
+      key     = "Value"
+      Example = "example"
     })
   }
 }
