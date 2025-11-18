d4ac08ba08638f8b8db41fd59d89b5ab089f8b62
PatrickMenoti <patrick.menoti@azion.com>
2025-11-18 16:23:29 -0300

    fix: update examples to match correct schema for edge firewall edge functions instance

--- examples/resources/azion_edge_firewall_edge_functions_instance/resource.tf
+++ examples/resources/azion_edge_firewall_edge_functions_instance/resource.tf
@@ -1,9 +1,10 @@
 resource "azion_edge_firewall_edge_functions_instance" "example" {
-  edge_firewall_id = 12464
-  results = {
-    name = "Terraform Test"
-    "edge_function_id" : 9359
-    "args" : jsonencode(
-    { a = "b" })
+  edge_firewall_id = "12464"
+  data = {
+    name     = "Terraform Test"
+    function = 9359
+    args = jsonencode({
+      a = "b"
+    })
   }
 }
