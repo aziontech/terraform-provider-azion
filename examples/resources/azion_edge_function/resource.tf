bf5ff8b98339435cea70de8c9afc6b8c5326b361
PatrickMenoti <patrick.menoti@azion.com>
2026-04-06 11:25:29 -0300

    chore: update test based on linter

--- examples/resources/azion_edge_function/resource.tf
+++ examples/resources/azion_edge_function/resource.tf
@@ -1,11 +1,11 @@
 # Example with inline code
 resource "azion_edge_function" "example" {
   edge_function = {
-    name                 = "Function Terraform Example"
-    code                 = "console.log('Hello World');"
-    active               = true
-    default_args         = jsonencode({ "key" = "Value" })
+    name                  = "Function Terraform Example"
+    code                  = "console.log('Hello World');"
+    active                = true
+    default_args          = jsonencode({ "key" = "Value" })
     execution_environment = "default"
-    runtime              = "nodejs20.x"
+    runtime               = "nodejs20.x"
   }
 }
