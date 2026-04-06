39708780793ead11f8f4b57287a90923b84574a5
PatrickMenoti <patrick.menoti@azion.com>
2026-04-06 11:21:46 -0300

    chore: update docs based on linter

--- examples/resources/azion_edge_function/resource.tf
+++ examples/resources/azion_edge_function/resource.tf
@@ -1,24 +1,8 @@
-resource "local_file" "content_file" {
-  filename = "${path.module}/example.txt"
-  content  = file("${path.module}/example.txt")
-}
-
-resource "azion_edge_function" "example1" {
+# Example with inline code
+resource "azion_edge_function" "example" {
   edge_function = {
     name                 = "Function Terraform Example"
-    code                 = trimspace(local_file.content_file.content)
-    active               = true
-    default_args         = jsonencode({ "key" = "Value" })
-    execution_environment = "default"
-    runtime              = "nodejs20.x"
-  }
-}
-
-
-resource "azion_edge_function" "example2" {
-  edge_function = {
-    name                 = "Function Terraform Example"
-    code                 = trimspace(file("${path.module}/example.txt"))
+    code                 = "console.log('Hello World');"
     active               = true
     default_args         = jsonencode({ "key" = "Value" })
     execution_environment = "default"
